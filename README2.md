b6873393de73ea638613c413140d6630234ff4c1
diedtho <diederichst@bfwob.de>
2022-08-10 22:37:33 +0200

    second commit

--- README2.md
+++ README2.md
@@ -2,3 +2,5 @@
 
 https://orcacore.com/set-up-use-anaconda-ubuntu-22-04/
 https://pyrosm.readthedocs.io/en/latest/installation.html
+https://stackoverflow.com/questions/43268156/process-finished-with-exit-code-137-in-pycharm
+
--- pyrosm_read_street_networks.py
+++ pyrosm_read_street_networks.py
@@ -1,16 +1,21 @@
 from pyrosm import OSM
 from pyrosm import get_data
+import osmnx as ox
 
 # Pyrosm comes with a couple of test datasets
 # that can be used straight away without
 # downloading anything
-fp = get_data("helsinki")
+fp = "./my_data/Helsinki.osm.pbf"
 
 # Initialize the OSM parser object
 osm = OSM(fp)
 
-# Read all drivable roads
-# =======================
-drive_net = osm.get_network(network_type="driving")
-drive_net.plot()
-print(type(drive_net))
+# Get all driving roads and the nodes
+nodes, edges = osm.get_network(nodes=True, network_type="driving")
+print(edges.head())
+
+# Create NetworkX graph
+G = osm.to_graph(nodes, edges, graph_type="networkx") # 'networkx' uses (too) much memory
+
+# Plot the graph with OSMnx
+ox.plot_graph(G)
